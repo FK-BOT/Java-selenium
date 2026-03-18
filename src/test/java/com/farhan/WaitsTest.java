d58d07d1c734c71ec6e224b86791fe4fed14b9e5
Farhan <farhankhan4@outlook.com>
2026-03-18 14:12:58 +0530

    added waitsTest

---
+++ src/test/java/com/farhan/WaitsTest.java
@@ -0,0 +1,56 @@
+package com.farhan;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.*;
+import java.time.Duration;
+
+public class WaitsTest {
+    WebDriver driver;
+
+    @BeforeEach
+    void setUp(){
+        driver=new ChromeDriver();
+    }
+    @Test
+    void implicitTest() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(12));
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.xpath("//button[text()='Start']")).click();
+        String text = driver.findElement(By.xpath("//div[@id='finish']/h4")).getText();
+        Assertions.assertEquals("Hello World!", text);
+
+    }
+
+    @Test
+    void explicitTest(){
+
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.xpath("//button[text()='Start']")).click();
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+        WebElement finish= wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='finish']/h4"))
+        );
+        String text= finish.getText();
+        Assertions.assertEquals("Hello World!",text);
+    }
+    @Test
+    void fluentWaitTest(){
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.xpath("//button[text()='Start']")).click();
+        Wait<WebDriver> fluent= new FluentWait<>(driver).withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(2))
+                .ignoring(NoSuchElementException.class);
+        WebElement finish=fluent.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='finish']/h4"))
+        );
+        Assertions.assertEquals("Hello World!",finish.getText());
+    }
+
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+    }
+}
