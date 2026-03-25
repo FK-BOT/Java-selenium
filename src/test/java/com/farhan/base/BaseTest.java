a0360fd40fe7e92f695fe4c9aeed0f893b8a6097
Farhan <farhankhan4@outlook.com>
2026-03-25 15:23:24 +0530

    fix: headless chrome and install chrome in CI

--- src/test/java/com/farhan/base/BaseTest.java
+++ src/test/java/com/farhan/base/BaseTest.java
@@ -1,6 +1,7 @@
 package com.farhan.base;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -10,7 +11,9 @@ public class BaseTest {
 
     @BeforeEach
     void setup(){
-        driver=new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
     }
